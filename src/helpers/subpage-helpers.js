7ddce997772aace09114bdd11dc10d7fe708703d
eelikivi <eelikivi@hotmail.com>
2019-08-18 01:26:46 +0300

    subpage-helpers fix

--- src/helpers/subpage-helpers.js
+++ src/helpers/subpage-helpers.js
@@ -1,14 +1,19 @@
 export default function setSubpageAfterBg() {
 	// set subpage's after elements bgcol to last subpage sections bgcol
 	const sections = document.getElementsByClassName('Subpage-section')
+	let styleTag = document.getElementById('SubpageSectionAfter')
 
 	if (sections.length > 0) {
 		const bgCol = window.getComputedStyle(sections[sections.length - 1], ':before')
 			.backgroundColor
-		const style = document.createElement('style')
-		style.innerHTML = `.Subpage:after {
-		background-color: ${bgCol};
-	}`
-		document.head.appendChild(style)
+		if (styleTag === null) {
+			const style = document.createElement('style')
+			style.setAttribute('id', 'SubpageSectionAfter')
+			document.head.appendChild(style)
+			styleTag = document.getElementById('SubpageSectionAfter')
+		}
+		styleTag.innerHTML = `.Subpage:after {
+			background-color: ${bgCol};
+		}`
 	}
 }
