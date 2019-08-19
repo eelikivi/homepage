4b78b1e863278c69440b79e7a8283609804c06a6
Eeli Kivikaarre <eeli.kivikaarre@lianatech.com>
2019-08-19 14:33:50 +0300

    Glitch animation

--- src/components/main/content/Hero.js
+++ src/components/main/content/Hero.js
@@ -7,7 +7,7 @@ export default class Hero extends Component {
 		return(
 			<section className="Hero jumbotron d-flex justify-content-center">
 				<div className="d-flex Hero__Title--container flex-column align-items-center">
-					<h2 className="Hero__Title font-lazer84"><span>Eeli</span> <span>Kivikaarre</span></h2>
+					<h2 className="Hero__Title font-lazer84 glitch">Eeli Kivikaarre</h2>
 					<h3 className="Hero__Subtitle font-bebasneue">Web developer</h3>
 					<ul className="Hero__Buttons list-unstyled d-flex">
 						<HeroButton
